1928fac8a050f8afbdb914535a813f0277a65e51
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-08-06 23:44:14 +0300

    ass5 initial commit

--- assignment3/run.py
+++ assignment3/run.py
@@ -45,7 +45,7 @@ def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=1
     ### Please see the following docs for support:
     ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
     ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
-    optimizer = optim.Adam(parser.model.parameters())
+    optimizer = optim.Adam(parser.model.parameters(), lr=lr)
     loss_func = nn.CrossEntropyLoss()
     ### END YOUR CODE
 
--- assignment4/README.md
+++
@@ -1,2 +0,0 @@
-# NMT Assignment
-Note: Heavily inspired by the https://github.com/pcyin/pytorch_nmt repository
